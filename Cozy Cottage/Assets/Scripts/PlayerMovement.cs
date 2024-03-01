0bd7e3ec845102319d0855dc6847bbc4d9b03bf3
TaekYung Jeong <tj29@students.calvin.edu>
2024-03-01 03:39:25 -0500

    'd'

--- Cozy Cottage/Assets/Scripts/PlayerMovement.cs
+++ Cozy Cottage/Assets/Scripts/PlayerMovement.cs
@@ -5,33 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    Animator animator;
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float sprintMultiplier = 2f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movementDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
         float currentSpeed = Input.GetButton("Sprint") ? movementSpeed * sprintMultiplier : movementSpeed;
 
-        rb.velocity = new Vector3(movementDirection.x * currentSpeed, rb.velocity.y, movementDirection.z * currentSpeed);
-
+        // Determine if the character is moving and set the animator speed parameter
         if (movementDirection != Vector3.zero)
         {
+            animator.SetFloat("speed", 1);
+            
+            // Rotate character to face movement direction
             Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
             transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
         }
+        else
+        {
+            animator.SetFloat("speed", 0);
+        }
 
+        // Apply movement
+        rb.velocity = new Vector3(movementDirection.x * currentSpeed, rb.velocity.y, movementDirection.z * currentSpeed);
     }
 }
